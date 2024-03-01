48696f1b4860f2ffff2f9327e1e0373879ec4254
16madhu <161235280+16madhu@users.noreply.github.com>
2024-03-01 05:43:23 -0800

    Add files via upload

---
+++ Additionofmatrix.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class Diagnoal{
+public static void main(String[] args){
+  int matrix[][]=new int[3][3];
+  int matrix[][]=new int[3][3];
+  Scanner inScanner=new Scanner(System.in);
